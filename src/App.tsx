82d5296888833ba9659233051e9286ae797a56e8
Gagi Shmagi <gagi.shmagi@gmail.com>
2021-09-05 11:57:56 +0300

    addded functional component type and props

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+interface AppProps {
+  name:string
+}
+
+const App:React.FunctionComponent<AppProps> = (props)  =>  
+(
+  <div className="App">
+  <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <p>
+      Hello {props.name} <br/>
+      Edit <code>src/App.tsx</code> and save to reload.
+    </p>
+    <a
+      className="App-link"
+      href="https://reactjs.org"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Learn React
+    </a>
+  </header>
+</div>
+)
+  
+
+export default App;
